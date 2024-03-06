ddd63e5b6dc431befd1f494b7f40286510462f27
Anderson <anderfrasao52@gmail.com>
2024-03-06 11:50:29 -0300

    .y

--- ProjetoBase/compilador.h
+++ ProjetoBase/compilador.h
@@ -14,13 +14,14 @@
 #define TAM_TOKEN 16
 
 typedef enum simbolos {
-  simb_program, simb_var, simb_begin, simb_end,
-  simb_identificador, simb_numero, simb_label, simb_type,
+  simb_program, simb_var, simb_begin, simb_end, simb_igual, simb_diferente,
+  simb_identificador, simb_numero, simb_label, simb_type, simb_menor_que,
   simb_ponto, simb_virgula, simb_ponto_e_virgula, simb_dois_pontos,
   simb_atribuicao, simb_abre_parenteses, simb_fecha_parenteses,
   simb_array, simb_procedure, simb_goto, simb_if, simb_else,
   simb_then, simb_while, simb_do, simb_or, simb_div, simb_and,
-  simb_not
+  simb_not, simb_menor_ou_igual, simb_maior_ou_igual, simb_maior_que,
+  simb_mais, simb_menos, simb_multi
 } simbolos;
 
 
